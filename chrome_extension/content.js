5ca4469d65bc53e110f6e565bac9817359d68702
Sven Kuhne <smkuhne@ucdavis.edu>
2020-01-18 19:19:56 -0800

    Paragraph bias small changes

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -32,13 +32,12 @@ Split(["#sidebar", "#webpage"], {
   sizes: [20,80]
 });
 
-var items = document.body.getElementsByTagName("*");
+var items = document.body.getElementsByTagName("p");
 for (var i = 0; i < items.length; i++) {
-    console.log(items[i].textContent);
     var container = document.createElement('div');
     container.setAttribute('style', 'display: flex; flex-direction: row; width: 100%')
     var bar = document.createElement('div');
-    bar.setAttribute('style', 'background-color:green; width: 20px; height: 100%; display: inline-flex');
+    bar.setAttribute('style', 'background-color:green; min-width: 10px !important; height: 100%; display: block; margin-right: 5px;');
     var inner = document.createElement('div');
     inner.setAttribute('style', 'display: block');
     inner.innerHTML = items[i].innerHTML;
@@ -46,4 +45,5 @@ for (var i = 0; i < items.length; i++) {
     container.appendChild(inner);
     items[i].innerHTML = '';
     items[i].prepend(container);
+    bar.style.height = `${inner.clientHeight}px`;
 }
